110d410674b75f4024a82c815f04bcda79087b84
Julien Dihal <juliendihal971@gmail.com>
2021-07-02 10:13:42 +0200

    wild card

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use http\Env\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -8,10 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController
 {
     /**
-     * @Route("/article", name="artcile")
+     * @Route("/articlelist", name="articlelist")
      */
- public function listeArticle(){
+ public function listeArticle()
+ {
      //retourne une reponse http valide
      return new Response('article');
+ }
+    /**
+     * @Route("/article/{id}", name="articlshow")
+     */
+ public function articleShow($id)
+ {
+
+ return new Response($id);
  }
 }
--- src/Controller/PokerController.php
+++ src/Controller/PokerController.php
@@ -4,26 +4,34 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
-class PokerController
+class PokerController extends AbstractController
 {
     /**
      * @Route("/poker", name="poker")
      */
 
-    public function poker()
+    public function poker(Request $request)
     {
 
-        $request = Request::createFromGlobals();
         $age = $request->query->get('age');
 
         if ($age >= 18) {
             return new Response('prend un sky ma poule');
         } else {
-            return new Response('degage minot');
+            return $this->redirectToRoute("digimon");
         }
 
+    }
+
+    /**
+     * @Route("/digimon", name="digimon")
+     */
+
+    public function digimon()
+    {
+      return new Response('digimon');
     }
 
 }
