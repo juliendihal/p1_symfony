a1e06e5390725b5c54b0bd84c516425366620522
Julien Dihal <juliendihal971@gmail.com>
2021-07-02 11:21:24 +0200

    affichage title id wild card

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Controller;
-use http\Env\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class ArticleController
@@ -21,7 +22,26 @@ class ArticleController
      */
  public function articleShow($id)
  {
+$article = [
+    '1' => [
+       "title" => "la vaccination c'est genial",
+        "content" => "blablabla",
+        "id" => 1
+    ],
+    '2' => [
+        "title" => "le cheval c'est trop genial",
+        "content" => "blablabla",
+        "id" => 2
+    ],
+    '3' => [
+        "title" => "le cheval c'est nul ",
+        "content" => "blablabla",
+        "id" => 3
+    ]
 
- return new Response($id);
+];
+
+$article = $article[$id];
+ return new Response( $article['title']);
  }
 }
