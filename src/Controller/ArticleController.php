729b66dc1059509a5b1999a2054e1946edee0619
Julien Dihal <juliendihal971@gmail.com>
2021-07-02 16:19:19 +0200

    css avec twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,38 +9,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
+public $articles =
+    [
+        '1' =>
+        [
+            "title" => "la vaccination c'est genial",
+            "content" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            "id" => 1
+        ],
+        '2' =>
+        [
+            "title" => "le cheval c'est trop genial",
+            "content" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            "id" => 2,
+
+        ],
+        '3' =>
+        [
+            "title" => "le cheval c'est nul ",
+            "content" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            "id" => 3
+        ],
+        '4' =>
+        [
+            "title" => "le cheval c'est nul ",
+            "content" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            "id" => 3
+        ]
+
+    ];
     /**
      * @Route("/articlelist", name="articlelist")
      */
 
  public function listeArticle()
  {
-        $articles =
-            [
-                '1' =>
-                     [
-                         "title" => "la vaccination c'est genial",
-                         "content" => "blablabla",
-                         "id" => 1
-                     ],
-                '2' =>
-                     [
-                         "title" => "le cheval c'est trop genial",
-                         "content" => "blablabla",
-                         "id" => 2
-                     ],
-                '3' =>
-                     [
-                         "title" => "le cheval c'est nul ",
-                         "content" => "blablabla",
-                         "id" => 3
-                     ]
-
-            ];
                 ////COMPILE LE FICHIER TWIG
              return $this->render('articlelist.html.twig',[
                    // RENVOI LES VARIABLES AU FICHIER TWIG
-                'articles'=> $articles
+                'articles'=> $this->articles
              ]);
 
 
@@ -50,37 +57,12 @@ class ArticleController extends AbstractController
      */
  public function articleShow($id)
  {
-        $articles =
-            [
-                 '1' =>
-                     [
-                         "title" => "la vaccination c'est genial",
-                         "content" => "blablabla",
-                         "id" => 1
-                     ],
-                 '2' =>
-                     [
-                         "title" => "le cheval c'est trop genial",
-                         "content" => "blablabla",
-                         "id" => 2
-                     ],
-                 '3' =>
-                     [
-                         "title" => "le cheval c'est nul ",
-                         "content" => "blablabla",
-                         "id" => 3
-                     ]
 
-            ];
                 //COMPILE LE FICHIER TWIG
-             return $this->render('articleshow.html.twig', [
+             return $this->render('articleshow.html.twig',  [
                  // RENVOI LES VARIABLES AU FICHIER TWIG
-                'article' => $articles[$id]
+                'article' => $this->articles[$id]
             ]);
-
-
-
-
 
  }
 }
